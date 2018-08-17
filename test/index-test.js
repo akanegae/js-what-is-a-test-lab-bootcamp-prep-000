9417facbf2ed15c9666a639d5db172b65e595830
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-17 03:20:26 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -15,7 +15,7 @@ describe('what-is-a-test', () => {
   })
 
   describe('Message', () => {
-    it('gives the name and height', () => {
+    it('Susan is 30 inches tall', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
